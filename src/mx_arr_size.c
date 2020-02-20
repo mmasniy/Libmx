91ea899de12bf97d667250e1f7b7ad726f3031d5
Maksim Masniy <mmasniy@e2r10p9.unit.ua>
2020-02-20 10:49:49 +0200

    add new func

--- src/mx_arr_size.c
+++ src/mx_arr_size.c
@@ -7,3 +7,25 @@ int mx_arr_size(char **str) {
 		size++;
 	return size;
 }
+
+int mx_allarr_size(char **str) {
+	int size = 0;
+
+	for (int i = 0; str[i]; i++){
+		size += mx_strlen(str[i]);
+	}
+	return size;
+}
+
+char *mx_strarr_to_str(char **strarr, int size_strarr, int i) {
+	char *str = mx_strnew(size_strarr);
+
+	while (strarr[i]) {
+		if (strarr[i] != 0) {
+			str = mx_strcat(str, strarr[i]);
+			str = mx_strcat(str, " ");
+		}
+		i++;
+	}
+	return str;
+}
